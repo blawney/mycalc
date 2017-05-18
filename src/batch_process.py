034594d5af713b43e77e4522171d6141edc8923d
brian <blawney@jimmy.harvard.edu>
2017-05-18 08:44:49 -0400

    Changing up the batch process script to accept a dataframe rather than a file path for more flexibility

--- src/batch_process.py
+++ src/batch_process.py
@@ -5,7 +5,18 @@ import models
 import os
 import pandas as pd
 
-def process_batch(input_file, eqn_file):
+def process_batch(df, eqn_file):
+    """
+    df is a Pandas DataFrame instance.
+    eqn_file is a formatted model file
+
+    It should have column names that match the species given in the model file so we 
+    can map the dataframe's values to the initial conditions properly.
+
+    Furthermore, the values in the dataframe should all have the same units-- we make no 
+    consideration for the relative units here-- that should all be cleared up prior to 
+    calling this function.
+    """
 
     factory = reaction_factories.FileReactionFactory(eqn_file)
     model = models.Model(factory)
@@ -21,38 +32,31 @@ def process_batch(input_file, eqn_file):
     col_set = set(df.columns.tolist())
     if len(col_set.difference(species_set)) > 0:
         pass # problem!
-    #df['ALB'] = df['ALB'] * 1e9 * (10./65000)
+    df['SHBG'] = df['SHBG'] * 0.5
+    df['Alb'] = df['Alb'] * 1e9 * (10./65000)
 
     def process(row, species_set):
+        """
+        A method for using with apply on the row axis
+        sets up the initiial conditions and runs until convergence
+        returns a pandas Series with the equilibrium concentrations of all species
+        """
         ic = {}
         for s in species_set:
             try:
                 ic[s] = row[s]
             except KeyError as ex:
                 ic[s] = 0.0
-        #ic = {'T': row['TT'], 'SHBG':0.5*row['SHBG'], 'Alb': row['ALB']}
+
         sample_to_column_mapping, solution, t = solver.equilibrium_solution(X0=ic)
-        #target_idx = ['T','AlbT','SHBGT','S1T', 'Tf']
-        #total_t = 0
-        #for t in target_idx:
-        #    idx = sample_to_column_mapping[t]
-        #    total_t += solution[-1,idx]
-        #total_t += 2*solution[-1, sample_to_column_mapping['SHBGT2']]
-        #print '%s, %s' % (row['TT'], total_t)
-        #for k,v in sample_to_column_mapping.items():
-            #print '%s: %s' % (k,solution[-1,v])
-            #print '*'*20
 
         final_vals = solution[-1,:]
         index = ['']*len(final_vals)
         for sample, col_idx in sample_to_column_mapping.items():
             index[col_idx] = sample
         s = pd.Series(final_vals, index=index)
-        #return solution[-1,sample_to_column_mapping['T']]
         return s
 
     results = df.apply(process, args=(species_set,), axis=1)
     df = pd.concat([df,results], axis=1)
-    print df
-    print '*'*20
     return df
